1c60be90f6f3a8cf1803a0f6595cd5ea899d1050
Eduardo Maia <eduvimaia@gmail.com>
2012-11-24 01:25:32 -0200

    inclusão das preposições

--- lib/googlon.rb
+++ lib/googlon.rb
@@ -9,4 +9,14 @@ class Googlon
   def self.bar?(letter)
     BAR.include? letter
   end
+
+  #
+  # as preposições são as palavras de 4 letras
+  # que terminam numa letra tipo bar,
+  # mas onde não ocorre a letra h.
+  #
+  def self.is_preposition?(word)
+    arr = word.split('')
+    return (arr.length == 4) && bar?(arr.last) && !arr.include?("h")
+  end
 end
--- spec/googlon_spec.rb
+++ spec/googlon_spec.rb
@@ -6,7 +6,7 @@ describe Googlon do
     it { Googlon::BAR.should == %w(a c d e f i j k l m n o p q r s t u x y w) }
   end
 
-  context "foobar" do
+  context "foo/bar" do
     it "its a foo letter" do
       Googlon::FOO.each do |letter|
         Googlon.foo?(letter).should be_true
@@ -17,6 +17,30 @@ describe Googlon do
       Googlon::BAR.each do |letter|
         Googlon.bar?(letter).should be_true
       end
+    end
+  end
+
+  context "prepositions" do
+    it "should have 62 prepositions in text a" do
+      text_a = File.read("spec/fixtures/text_a.txt")
+
+      preps = 0
+      text_a.split(" ").each do |word|
+        preps +=1 if Googlon.is_preposition?(word)
+      end
+
+      preps.should eql(62)
+    end
+
+    it "should have 65 prepositions in text b" do
+      text_b = File.read("spec/fixtures/text_b.txt")
+
+      preps = 0
+      text_b.split(" ").each do |word|
+        preps += 1 if Googlon.is_preposition?(word)
+      end
+
+      preps.should eql(65)
     end
   end
 end
