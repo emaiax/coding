f713e4e623f8b2f1a24b0aa54d5a87805754e0c8
Eduardo Maia <eduvimaia@gmail.com>
2013-05-14 00:28:03 -0300

    project euler - problem 3 [solved]

--- ruby/project_euler/problem3.rb
+++ ruby/project_euler/problem3.rb
@@ -3,7 +3,14 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143 ?
 
+require 'prime'
+
 module Euler
   class Problem3
+    class << self
+      def primes_of(number)
+        Prime.prime_division(number).flatten.uniq.sort
+      end
+    end
   end
 end
--- ruby/spec/project_euler/problem3_spec.rb
+++ ruby/spec/project_euler/problem3_spec.rb
@@ -3,4 +3,14 @@ require './project_euler/problem3.rb'
 
 describe Euler::Problem3 do
   subject { described_class }
+
+  describe "primos" do
+    it "retorna os números primos de 13195" do
+      subject.primes_of(13195).should == [1, 5, 7, 13, 29]
+    end
+
+    it "retorna o maior número primo de 600851475143" do
+      subject.primes_of(600851475143).last.should eql(6857)
+    end
+  end
 end
