fc1d7b83f5bd0ba9cec2c478ba8f0d24edd24a27
Eduardo Maia <eduvimaia@gmail.com>
2013-05-14 00:48:49 -0300

    project euler - problem 4 [solved]

--- ruby/spec/project_euler/problem4_spec.rb
+++ ruby/spec/project_euler/problem4_spec.rb
@@ -3,4 +3,14 @@ require './project_euler/problem4.rb'
 
 describe Euler::Problem4 do
   subject { described_class }
+
+  describe "números palíndromos" do
+    it "retorna o maior palíndromo dos produtos de números de 2 dígitos (até 99)" do
+      subject.palindromes_of_multiples_up_to(99).last.should eql(9009)
+    end
+
+    it "retorna o maior palíndromo dos produtos de números de 3 dígitos (até 999)" do
+      subject.palindromes_of_multiples_up_to(999).last.should eql(906609)
+    end
+  end
 end
