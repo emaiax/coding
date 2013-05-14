0a8d8bd56ccb9dd116d6641caa8939aae1ec97bf
Eduardo Maia <eduvimaia@gmail.com>
2013-05-14 00:48:49 -0300

    project euler - problem 4 [solved]

--- ruby/project_euler/problem4.rb
+++ ruby/project_euler/problem4.rb
@@ -7,5 +7,16 @@
 
 module Euler
   class Problem4
+    class << self
+      def palindromes_of_multiples_up_to(term)
+        [].tap do |arr|
+          (1..term).each do |n|
+            (1..n).each do |i|
+              arr << n*i if (n*i).to_s == (n*i).to_s.reverse
+            end
+          end
+        end.uniq.sort
+      end
+    end
   end
 end
