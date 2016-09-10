882a4cdd3e1e53c869eca070fa8166c9075682b4
Eduardo Maia <eduvimaia@gmail.com>
2016-09-10 07:25:56 -0300

    typo

--- codility/lesson_001/ruby/binary_gap.rb
+++ codility/lesson_001/ruby/binary_gap.rb
@@ -2,7 +2,9 @@
 
 A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
 
-For example, number 9 has binary representation 1001 and contains a binary gap of length 2. The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3. The number 20 has binary representation 10100 and contains one binary gap of length 1. The number 15 has binary representation 1111 and has no binary gaps.
+For example, number 9 has binary representation 1001 and contains a binary gap of length 2. The number 529 has binary representation 1000010001 and contains
+two binary gaps: one of length 4 and one of length 3. The number 20 has binary representation 10100 and contains one binary gap of length 1.
+The number 15 has binary representation 1111 and has no binary gaps.
 
 Write a function:
 
@@ -14,12 +16,12 @@ For example, given N = 1041 the function should return 5, because N has binary r
 
 Assume that:
 
-N is an integer within the range [1..2,147,483,647].
+  - N is an integer within the range [1..2,147,483,647].
 
 Complexity:
 
- - expected worst-case time complexity is O(log(N));
- - expected worst-case space complexity is O(1).
+  - expected worst-case time complexity is O(log(N));
+  - expected worst-case space complexity is O(1).
 
 Results:
 
