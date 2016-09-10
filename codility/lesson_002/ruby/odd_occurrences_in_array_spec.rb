b09758fd81737c6b0356c7b7a9d7b0534b2cecc5
Eduardo Maia <eduvimaia@gmail.com>
2016-09-10 07:00:24 -0300

    added codility lesson 2: odd occurrences in array

---
+++ codility/lesson_002/ruby/odd_occurrences_in_array_spec.rb
@@ -0,0 +1,69 @@
+=begin
+
+A non-empty zero-indexed array A consisting of N integers is given. The array contains an odd number of elements, and
+each element of the array can be paired with another element that has the same value, except for one element that is left unpaired.
+
+For example, in array A such that:
+
+  A[0] = 9  A[1] = 3  A[2] = 9
+  A[3] = 3  A[4] = 9  A[5] = 7
+  A[6] = 9
+
+  - the elements at indexes 0 and 2 have value 9,
+  - the elements at indexes 1 and 3 have value 3,
+  - the elements at indexes 4 and 6 have value 9,
+  - the element at index 5 has value 7 and is unpaired.
+
+Write a function:
+
+  def solution(a)
+
+that, given an array A consisting of N integers fulfilling the above conditions, returns the value of the unpaired element.
+
+For example, given array A such that:
+
+  A[0] = 9  A[1] = 3  A[2] = 9
+  A[3] = 3  A[4] = 9  A[5] = 7
+  A[6] = 9
+
+the function should return 7, as explained in the example above.
+
+Assume that:
+
+  - N is an odd integer within the range [1..1,000,000];
+  - each element of array A is an integer within the range [1..1,000,000,000];
+  - all but one of the values in A occur an even number of times.
+
+Complexity:
+
+expected worst-case time complexity is O(N);
+expected worst-case space complexity is O(1), beyond input storage (not counting the storage required for input arguments).
+
+Elements of input arrays can be modified.
+
+Results:
+
+  - Test score: 100%
+  - Correctness: 100%
+  - Performance: 100%
+  - Link: https://codility.com/demo/results/training39FXWV-YE9/
+
+=end
+
+require_relative "../../../spec_helper"
+
+def solution(a)
+  list = Hash.new(0)
+
+  a.each { |i| list[i] += 1 }
+  list.detect { |_, v| v.odd? }.first
+end
+
+RSpec.describe "OddOccurrencesInArray" do
+  it { expect(solution([42])).to eql(42) }
+
+  it { expect(solution([9, 3, 9])).to eql(3) }
+  it { expect(solution([9, 3, 9, 3, 9, 7, 9])).to eql(7) }
+
+  it { expect(solution(Array.new(999_998, 1) << 2)).to eql(2) }
+end
