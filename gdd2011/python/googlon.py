5ff3de5befe80b5a7d853c5c947346636c71793d
Eduardo Maia <eduvimaia@gmail.com>
2017-01-18 15:53:18 -0200

    GDD2011 python implementation

---
+++ gdd2011/python/googlon.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# GDD 2011
+#
+# Arqueólogos encontraram um pergaminho com os seguintes textos:
+#
+text_a = '''zcjjttpt njfqrzxj pgwd dknttlb tdbsgf plphnnv kpmmgxb gkhp fgrfb dxss cdswmbgb cvhxd jzrr kwchwcv kqzrlxz zmnc cmsqdjx chbb lnt dgrhmgn lwlqdh wtfh ndtbsqsh
+xftg rqkr wcptgbc qslm cxpplqbq sbst cgrtr bplzzpjw hrdqrds nfkltv bbdqpvn jkk zhzvm cpcnpnww wffrgzp gsrxcb gmv jvvc zbmclc kcdwcpdm kmk xtdzrz mxjccgv jvcmkbxv khzzw vcj
+tjgmjzh gvhkcsvw czhhtclr wxxchz hhcxnnhn vqbq vmhp hlnls wrsctd khlbtg jbnw fsp dwsk jzzpb jbnplx tgnq tdjzcwj vmd gjvnj nmc blnw ghfntj hdslhv jplzc ggzb djbgqqk szbmqd
+zjqcbxf crbhzfs dgwd jzdfqq wrzct hbff tnt wdxmfr lmbtkhch hdj rzp mxqhpg trzz vmpdxkv bjgllxv lwdbkws hlkxtc gcmdfzdx qzj mhj skkfq rgtrj smdlrm mhlrmd xgj gcnzl dxpx xmtzz
+fvjdqd xrqhp wzb fqppvgk xvgkv lxtf dwlrgxtm kbg ksptr pqc wnlmw ljxh jfmcl hqlgx pljpvcn hzdb dcrzslrf lzhr gkt lmq blx ppzpvfbv jtwhxdx nlbcwrvv mtrfm bkjcm qnghqcch
+fgvsxzp lxkmnzx fpj vxpbxrdj gtvrwp rrflt lgh ngkgnln tgq qddg rsqb dcxsrkl zwvwrnn zbrpktxj rbr dmp qsfctn dxmkk nmmqnzfn zqwb dtrsfb gzrq lbqjrp cstjtb xxzdlwzx zdrfjwc
+rhhmzvp tgjpq tpppxf xffjdlfs ngwrbrfk lxzscps dcsbhwp mrddntg jlgz dkzxctcn pkqxrsw rjldhzld rflzw gqhdfchv hsc kknhkq xdlsm cgcdvbpt vftnnd ttjtx bzrznlh xggzgkln wtlsx
+hphvdvl vjmmf trxl rwtxljp mqfsrwt hljqwkz skwkh gjbbg cwkc rfqdh jntktdz tkljpkfc fgmcd lnkfqjt ggvf lgf qfbrdg xrsrjv krdb pmnt pjcjzct tplwbr wpfl dsgnhsf fdh cmvjrf
+ccx zgnlpgwl rkvg qhvszv rqm pbmp vjq wfqxrnzf frp rcfxhhh hbtjpvm dckqls brprszrc tpvdrlf bbzvs rsd bmzqtdx spzzbc mfhllb dgsxl gvwg fpbwbzb wbht jbw psjc pdhqn wdc xfpj
+ftfpd cdxsgqgs vkvqhk htkmdbx knhmmtks jhztz jsfkd pfkjshg rxrmqp jjqn kkrr kswfwbzv qgsvbgws sfwsnkkw tzwvbkfq vglkdghk jlhpgh wknwbcq rwtzbhs qcxr ncrmzr lmbfvsv rtb
+rkzlhbnx mcld mpqxtbzb qlpt fdsfsc rfqsr tvphkg xldsnqds zlmtqs vmqzmg bvrjztfm mkfsfcwd mmj dxz mkszj dcgxxcqm kvtgzpl skxpc dggshkxz cncdbm bsb mbd wcc bfjmw wjg
+qmwknt jgl fvntg mkmnwtzv sbrkdjmh kszjq jgw jfxzx krh svztn pxcpvkmm wbcjprr bslbmwm skhcwxw jxnng fwqwfxs dtblv cgp lmbjjx mlsm cgpn hhrrx gvbfxtbv tzhqvbt xxtlt vdszkb
+jbmlsfkx jsfpllns nfhvn mkt twjhgcc kwvnrfm ltjwn lbrnrsf zgqxqpvm ptmnzqkg mbzkdw gxglr wzfsf hcr pdfqlsv vdbbxdtw ptg rwjls qlmhxrrf ngsbphqb lphf vghjwkvl rdrphff
+hwfbd hffjd nrpzqfr ckxtmqx mfm pfcgz szzh srwg phk tmjlqw gwpcph qbpsv mmksm tgmwmsc mbccsd qdr lmknq tjw lljs plkxf vxf pnhc gkh gjw xqbs kslnx sbnfjs gtrpzhwb bdjxr xhjk
+cbk whnjkv cmgp cmrgdh qdht wtsp tzqcvz vth zflwbn wzg wthwv sxcdqtlt bwfgzmjn vfb zldk chv wptc ztvkmv brxk lrljqdct gsqfgjn lzgfnkmg zgl gqwxpncw bfjbs tqbs mclvp znktlpx
+dfvt ltqxkt gqjjcbjw pmgtzlng gfc nfv mdkc tnbqgv lbbkssx frtvg rtxct njqw mdrgqdht hgsx lqncvrh djftp tzd vsrp wcpn pgbkhc ppsp mjhskxjh dvmj nwjjkrdq hxr wkq fjmt cbkgf
+txz tsmrhhtm vttn kmbgdwm ljdz nqbh zwwx jtmdmt pznlvfk kpbslq mdxlzvkg znngdwb vsng gxltm llshm tjpdp cnpqtw nmdzgmnr vqdmjdsb rgpgqz cwmb nvb wlj xpbddsjw gbprtv djsv
+bhrfvh bpngkf fcppm xvqnd pctgq qjlmkkmv njv qcrx vwddsdl lbkw xblfrb bghqpnl nkjk lqmtsnrn bdnfbh pmmd jfhhv tbw ftzkqg kgj cqw wmvjxdsw xwxjhbc khhmfng hmnf qjvg knlwvk
+trvpx lzq dfj fzlxll lllkssb vvsvkq mlfmddsn kwhcrd dlbnhz hlxx kcxl nbkvsrf krprtxf wwmngbk jmqkbclx zjcrkzm pfcpt mhcskk mjwrncd vkffrqvt dfctpgr dqfjwtv kcr dfbnht
+pcvpwjb zzrr gpn njq tvpjwzb ttl nvmfhshh twxcfxk lvqckrl lsmphpk fkzql lvq rmmhtmbp mqdgvskt lfhkmb sklfrc pvf kzzsbq qbvvz pdnqm jmstqwx bzqcnzwp grplms whshcrk tgsffh
+zvtnbs khqr xlc wlsxsvf jqbcddqh gnksf smtkrxjx nqjflf jxsd tdsbdhv htldnxcn szltzjm nsx lfjvrhr frs wrgf srtfttv pqsq swff rskfms bzccxd qzz mdcqlbwx frskk wcjlrs mdvcfqkt
+pwpznr qwl jqjgd gcnkszfk ptmzl mdz pgjjhr phbq hzrv tlthnhrh jcw vwmb zbdbsq lvwht jhtxc ktbcx dpl pdxjxpxr zndtfmf cltdd fwwm ngqzs kgzsb xxcfpp bmt kztrcqxt jvkwr brzxg
+gthkq pphslgf ftdqspbf vhxdj fkmrtn rtm gscvghmj rpxwnbrw vgclcmdx zkf qdgqls nlvgr vcvccs jbvqmb lqg zxljnd phn ztm qkkjgg bzfqkpjj gbn pcl tgtclv fshm ngc zwwdsd ggd'''.split()
+#
+text_b = '''clnr bcdktps fvxsmsbx kqj hvbncjw wsmngb xhcvvc nfkjfn fkvl ljhqlbhs ptqtwp vtfbq szkpmn hxl hsd qmrr jzjbjgp tsssrk fmvx bstbzwsx njdm nrvfgs bdjzlg nwnlmbx
+vjqqxsp vbxj gwtll xfrmgqqj mmttm xlbnkbw bkw frgwz twrkx nfgxqmb cvr tgmw nfw mghts bsncq zfn vpkgwcd cdbdwsjf krrkl fst lmz vvwtkrf dqt fkm pfnpqh vdslwsdk kbmfjgs fkwkbd
+nssd clbpzcpd qph ksfgcvw mcs nbjbkrz jbtcbqm lpj wvsscb tpqnm gswg gtd dpf shztl brgkfqnf xgw fsjmrvcx qzd slt xhhg vxgcfc hxfq jhxpngr nmpx gjdn kgmq hpb klzsxz
+bpcqrhq wrnn mgthn wzjcvj pgft vtksnpbb qtlmgsh nrzdkx lvd kpr fwrgdmjt jzknzgk vqmkgkb mptrq rslljk trggnlpd fcffvg wshnn tvq skddwrtg srd gbwdh pvgkrhd qndpq phmhxkck vkgfc
+mzpp gph nzn lcxc jpnx mxrg xpjkjxc fkhr fqfqcjtd bzmvqp wlsdx txlttnpb vdb mxnswm dwqnsgj mxg xmszj bdttl xmwth nfchzb vtlxg lqmxbx sgs hnw zgdsp qcgpc xhk pwbfdmtc
+ftkgv hqntlps dgbwpk jzsgkb kcsb xjnjhgh ckx pxzm tlpzlxj bjdd rjjp mxqxqdxc kfvg mqpvxk cmg jgz fmzf bnr fvfgnzx crkxcs zszmrfjv qsjgzzzp lcgsgjvh znjntxpj hdqzjc
+tmzxrrg nqlnsk mwtlm cvdk vqbhj wbpdssgm nsnv nhfptrsg vjgrlfs zkvkdxz brzbhlns pfs cvrjxjq fbbkvdhr bhtlqhvv rhjtsvv gfbrqn mvclz ghm sgkk tcmfz dtrmkn dzcjppjm mlpkx
+qbqbvpsm xxd vrqq sjwwc pchqk jnwp txk pvf dpff lqrdrz ncv mwgf chnz rjlfpch rnvdjpc mfxs shdf nfpnlr rvqw zmllbxs sfbvz hbcv bgp jtg bgsfnz hgkkwd nnzbqwgs lktdlrlx qxrs
+dpcj kwfj tjsh rnxhwgd dpmdnz xgnfggb pfrxglb plzxjqlk whts jgrt clvtxn mhnhb wndwxs wlkdtjz ghhmdq bcw vvlhntpt jnzznp bksx mvssxl kjdkt pjjzqkvx vwtbh rkkxqk xbwknvmr
+nmddl rnwqq frfbhk ctvfgxzv gplktxj lljfz lntr bndwhjwp tvr gbjz lbjrnmt hzjwqn wnxsmnx mjxh hlsssh nczkp wtbfv ztbcph cnhgxsd qddzdv ktzb jwhvvrtr qlrhnww zld cdvr xvsbdw
+lvsbxzv cslq cjvrwg sdvnw hcr stnhs nsxmr npqqm psr gcgg zpvrdnc qgzf qxp npfvr rzv ptkp drrlpnr dpptqzc vbf tdhps crjnct gvc fxt hckr vjhlfgld rfkvbr fbbhqvg kgzcf gsxm
+fvjkmj xmchrvwx dcxggwhc fmt jwvbhl ppldgl mfxmqjn tvcgnrb zssswpc ghhq dvsl vlj rqrsw pbvtkm kdtxsj qlgpv xtm rncp smnrwmlv kbndtscg gtps wbdxk qbm rqprpvj stgwsc lqklxr
+bxqhw hfhp lbrhbn klhtb wslrbz lpwqpn znsxfqs ldzzlkg hphjhns thc vddhjkq dpzrl mqt lvmptxcd bfxlg hnss bgncx slw rpgtkzz jgngwzc vjxgbs npdd hpzmp xfmnsjc scqskgsl dzxpk
+bwxr tbxwpk svtlgdl dzsvqbnx wjfktwv qtwhllw nndtsx hxmjpmnv cnxck cmh kccw hdfrtd qzcsksd dljps xcfstz rvfqp hrwx pslzhzhc bkwfwfnj phcxvpf lpwl dzftkjr vpvwnjtg srhnz
+hwlqvc dsrdq ntk vnxr vbmd kctx jhg ptfhnlc xxtctgsj pwj pxwt fzzx zzzfchrk qznnxl gdzj rjd pkj jlmwtcs twgzh ttmgnwb hwjbnqwv zgblvhlj bgg sqhfcnlk slkbcmj nwmm hgjjksxf
+drfrgjx hbjvpw rjrlrfk hcv frp rbsfrrcf jtkqhh kjtqpxhw hxhjznc jgxn dtlsp llw xvrvsdf cgfwq wrmv kcnr mpqnqr dqtswd qnnxhm jsl njxtbh zcbpcbpt bdllcsdl qbxwpg mlgtjw zbgsxg
+lbcxxgsf cbwjfldn rdp vjwsjpw srvc rkln bddvc gbgw nckdtf ckvmtbvf cwnf cnsmqxwn zvxhgq mgj pxv jms hbjr fpvxzwmx srlml rzfmdp xtxsblgt gwbvj krnn lthk dcx vhpbdwd rtvmzn
+bcclbzz hcdp qlw mxpwc lxccgcxh zhprp rsfpxl pmznqzh nbbkqjt cbmhp hbtn vlgfskcx cwh jtxhfvr jjc ttjcc cqlphsk mtgnc bdr xvpztf sxpb fxh gpgpqtrc pvpcmx wvcsmb lkd qxpm
+tdbxnwrg wvcpw hwswwqg bhkwfwxm cbvwhf bgnvwqln fgn ntnhcl ffq btzjd pbzqnc thfdcpxt rzmjrbfb lcrdlc tqlqrf ffkbj kqt qnkfd jwbt rgc zdbsvmll pfvnpwj ppq kltqfx klmjxg
+qpnnjwl xdvs qdvjskx dcnhhltm kqwxr xrzwj prmqclss xvhcb lfn hlcnqnw wbnxl nmjkkmpg gth gbb xsnhn qgjs xlt nmzhrrqn bkrhjtsl rvqzhmm lzlbc lmpfkk xtkdp ckm vhbnd kmhj xzrz
+gwdxkhr xnk jwnwz knbhrwgs dfcbw nfgkxsw fbg rntpnh mvkfdhh rcltszw lhld plr xhqdvhmp xhxsqp zqmsnl wkfxqzkx rdzhzx znzppsg hmqhxfpb nnmr kfpqcpx zbfck sdqxsnw cvl vsdrhj
+pbmlw gvp pwlhnpgf rrpzcwp pmcmrmvf fttbf zgtkjdm ddqmr twtksl vkdns rffn vkjnkkv hslbhksz glzb grwpg szzw rwmbvt grtkzrwh nwkjt tkhnb wbswcvbh mzmlgpp gggck sdcptlln
+gqz vpkpbsn nnw pnbqqbk mrgnflhr tchctgjn zmfkxvms kvqwc kgsh jfdjq mqndvm kckksgp wrrdnmjz bjjcsvms dvfqjqsf wps ngrngr rspxz bnvkmhcl kxgdbxhh kbcbg dmzwnfgm qnmtrvx kqwjtrcg'''.split()
+#
+# Esses pergaminhos estão no antigo e misterioso idioma Googlon. Após muitos anos de estudo, os lingüistas já conhecem algumas características desse idioma.
+#
+# Primeiramente, as letras Googlon são classificadas em dois grupos: as letras "b, c, t" são chamadas "letras tipo abacate", enquanto que as demais são conhecidas
+# como "letras tipo outras".
+#
+# Os linguistas descobriram que as preposições em Googlon são as palavras que começam com uma letra tipo abacate, e que também terminam com uma letra tipo abacate
+# é fácil ver que existem 15 preposições no Texto A.
+#
+# QUESTÃO A) E no Texto B, quantas preposições existem?
+#
+# Um outro fato interessante descoberto pelos linguistas é que, no Googlon, os verbos sempre são palavras de 7 letras que terminam numa letra tipo outras.
+# Além disso, se um verbo começa com uma letra tipo outras, o verbo está em primeira pessoa.
+#
+# Assim, lendo o Texto A, é possível identificar 84 verbos no texto, dos quais 72 estão em primeira pessoa.
+#
+# QUESTÃO B) Já no Texto B, quantos são os verbos?
+# QUESTÃO C) E quantos verbos do Texto B estão em primeira pessoa?
+#
+# Um professor universitário utilizará os textos A e B para ensinar o Googlon aos alunos. Para ajudar os alunos a compreender o texto, esse professor precisa criar
+# uma lista de vocabulário para cada texto, isto é, uma lista ordenada das palavras que aparecem em cada um dos textos.
+#
+# Essas listas devem estar ordenadas. No Googlon, assim como on nosso alfabeto, as palavras são ordenadas lexicograficamente, mas o problema é que no Googlon,
+# a ordem das letras no alfabeto é diferente da nossa. O alfabeto Googlon, em ordem, é: bctplmrsfzhjnwxkqvdg. Assim, ao fazer essas listas, o professor deve
+# respeitar a ordem alfabética Googlon.
+#
+# O professor preparou a lista (ordenada) de vocabulário para o Texto A:
+#
+vocabulary_a = '''bbzvs bbdqpvn bplzzpjw bpngkf blnw blx bmt bmzqtdx brprszrc brzxg brxk bsb bslbmwm bfjbs bfjmw bzccxd bzrznlh bzfqkpjj bzqcnzwp bhrfvh bjgllxv bwfgzmjn bkjcm
+bvrjztfm bdjxr bdnfbh bghqpnl cbk cbkgf ccx cpcnpnww cltdd cmrgdh cmsqdjx cmvjrf cmgp crbhzfs cstjtb czhhtclr chbb chv cncdbm cnpqtw cwmb cwkc cxpplqbq ckxtmqx cqw cvhxd
+cdswmbgb cdxsgqgs cgcdvbpt cgp cgpn cgrtr tbw ttl ttjtx tpppxf tplwbr tpvdrlf tlthnhrh tmjlqw trzz trxl trvpx tsmrhhtm tzhqvbt tzwvbkfq tzqcvz tzd tjpdp tjw tjgmjzh tnbqgv
+tnt twjhgcc twxcfxk txz tkljpkfc tqbs tvphkg tvpjwzb tdbsgf tdsbdhv tdjzcwj tgtclv tgmwmsc tgsffh tgjpq tgnq tgq pbmp pctgq pcl pcvpwjb ptmzl ptmnzqkg ptg ppsp ppzpvfbv
+pphslgf plphnnv pljpvcn plkxf pmmd pmnt pmgtzlng psjc pfcpt pfcgz pfkjshg pznlvfk phbq phn phk pjcjzct pnhc pwpznr pxcpvkmm pkqxrsw pqc pqsq pvf pdfqlsv pdhqn pdnqm pdxjxpxr
+pgbkhc pgjjhr pgwd lbbkssx lbrnrsf lbkw lbqjrp ltjwn ltqxkt lphf lllkssb llshm lljs lmbtkhch lmbfvsv lmbjjx lmknq lmq lrljqdct lsmphpk lfhkmb lfjvrhr lzhr lzq lzgfnkmg
+ljxh ljdz lnt lnkfqjt lwlqdh lwdbkws lxtf lxzscps lxkmnzx lqmtsnrn lqncvrh lqg lvwht lvq lvqckrl lgf lgh mbccsd mbzkdw mbd mclvp mcld mtrfm mpqxtbzb mlsm mlfmddsn mmj mmksm
+mrddntg mfm mfhllb mhcskk mhlrmd mhj mjhskxjh mjwrncd mxjccgv mxqhpg mkt mkmnwtzv mkszj mkfsfcwd mqfsrwt mqdgvskt mdcqlbwx mdrgqdht mdz mdxlzvkg mdkc mdvcfqkt rbr rcfxhhh rtb
+rtm rtxct rpxwnbrw rmmhtmbp rrflt rskfms rsqb rsd rflzw rfqsr rfqdh rzp rhhmzvp rjldhzld rwtzbhs rwtxljp rwjls rxrmqp rkzlhbnx rkvg rqm rqkr rdrphff rgtrj rgpgqz
+sbrkdjmh sbst sbnfjs spzzbc smtkrxjx smdlrm srtfttv srwg sfwsnkkw szbmqd szltzjm szzh swff sxcdqtlt sklfrc skhcwxw skwkh skxpc skkfq svztn fcppm ftfpd ftzkqg ftdqspbf
+fpbwbzb fpj frtvg frp frs frskk fsp fshm fzlxll fjmt fwwm fwqwfxs fkmrtn fkzql fqppvgk fvjdqd fvntg fdsfsc fdh fgmcd fgrfb fgvsxzp zbmclc zbrpktxj zbdbsq zcjjttpt ztm ztvkmv
+zlmtqs zldk zmnc zflwbn zzrr zhzvm zjcrkzm zjqcbxf znngdwb znktlpx zndtfmf zwwx zwwdsd zwvwrnn zxljnd zkf zqwb zvtnbs zdrfjwc zgl zgnlpgwl zgqxqpvm hbtjpvm hbff hcr htldnxcn
+htkmdbx hphvdvl hljqwkz hlnls hlxx hlkxtc hmnf hrdqrds hsc hffjd hzrv hzdb hhcxnnhn hhrrx hwfbd hxr hqlgx hdslhv hdj hgsx jbmlsfkx jbnplx jbnw jbw jbvqmb jcw jtmdmt
+jtwhxdx jplzc jlhpgh jlgz jmstqwx jmqkbclx jsfpllns jsfkd jfmcl jfhhv jfxzx jzrr jzzpb jzdfqq jhtxc jhztz jjqn jntktdz jxsd jxnng jkk jqbcddqh jqjgd jvcmkbxv jvkwr jvvc jgl
+jgw nbkvsrf ncrmzr nlbcwrvv nlvgr nmc nmmqnzfn nmdzgmnr nrpzqfr nsx nfhvn nfkltv nfv njfqrzxj njq njqw njv nwjjkrdq nkjk nqbh nqjflf nvb nvmfhshh ndtbsqsh ngc ngsbphqb
+ngwrbrfk ngkgnln ngqzs wbcjprr wbht wcc wcptgbc wcpn wcjlrs wtlsx wtsp wtfh wthwv wptc wpfl wlsxsvf wlj wmvjxdsw wrsctd wrzct wrgf wffrgzp wfqxrnzf wzb wzfsf wzg whshcrk
+whnjkv wjg wnlmw wwmngbk wxxchz wknwbcq wkq wdc wdxmfr xblfrb xtdzrz xpbddsjw xlc xldsnqds xmtzz xrsrjv xrqhp xftg xfpj xffjdlfs xhjk xwxjhbc xxcfpp xxtlt xxzdlwzx xqbs
+xvqnd xvgkv xdlsm xgj xggzgkln kbg kcr kcxl kcdwcpdm ktbcx kpbslq kpmmgxb kmbgdwm kmk krprtxf krh krdb ksptr kslnx kszjq kswfwbzv kztrcqxt kzzsbq khlbtg khzzw khhmfng khqr
+knlwvk knhmmtks kwchwcv kwhcrd kwvnrfm kkrr kknhkq kqzrlxz kvtgzpl kgzsb kgj qbpsv qbvvz qcrx qcxr qlpt qlmhxrrf qmwknt qslm qsfctn qfbrdg qzz qzj qhvszv qjlmkkmv qjvg
+qnghqcch qwl qkkjgg qdr qdht qddg qdgqls qgsvbgws vcj vcvccs vttn vth vmpdxkv vmhp vmqzmg vmd vsrp vsng vfb vftnnd vhxdj vjmmf vjq vwmb vwddsdl vxpbxrdj vxf vkffrqvt
+vkvqhk vqbq vqdmjdsb vvsvkq vdbbxdtw vdszkb vgclcmdx vglkdghk vghjwkvl dcrzslrf dcsbhwp dcxsrkl dckqls dcgxxcqm dtblv dtrsfb dpl dlbnhz dmp dsgnhsf dfbnht dfctpgr dfj dfvt
+djbgqqk djsv djftp dwlrgxtm dwsk dxpx dxmkk dxss dxz dkzxctcn dknttlb dqfjwtv dvmj dgrhmgn dgsxl dgwd dggshkxz gbprtv gbn gcmdfzdx gcnzl gcnkszfk gtrpzhwb gthkq gtvrwp
+gpn gmv grplms gscvghmj gsrxcb gsqfgjn gfc gzrq ghfntj gjbbg gjw gjvnj gnksf gwpcph gxltm gxglr gkt gkh gkhp gqhdfchv gqjjcbjw gqwxpncw gvbfxtbv gvhkcsvw gvwg ggzb ggvf ggd'''.split()
+#
+# QUESTÃO D) Como seria a lista de vocabulário do Texto B?
+#
+# Mas como os Googlons escrevem números? Bem, no Googlon, as palavras também são números dados em base 20, onde cada letra é um dígito, e os dígitos são ordenados
+# do menos significativo para o mais significativo (o inverso do nosso sistema, para ajudar no seu programa). Ou seja, a primeira posição é a unidade, a segunda
+# posição vale 20, a terceira vale 400, e assim por diante. Os valores das letras são dados pela ordem em que elas aparecem no alfabeto Googlon (que é diferente da
+# nossa ordem, como vimos acima). Ou seja, a primeira letra do alfabeto Googlon representa o dígito 0, a segunda representa o dígito 1, e assim por diante.
+#
+# Por exemplo, a palavra "mrsft" tem o valor numérico de 386925. Faça uma função que calcule o valor numérico de uma palavra Googlon para ajudar na sua lógica.
+#
+# Os Googlons consideram um número mágico (muito raro) se ele satisfaz as propriedades: o valor numérico é divisível por 13 e o número de dígitos na palavra original
+# é par e menor que 7 posições.
+#
+# Ao considerarmos o Texto A como uma lista de números (isto é, interpretando cada palavra como um número usando a convenção explicada acima), notamos que existem 16 números mágicos:
+# fshm xrsrjv mdkc zmnc pgbkhc rgpgqz cstjtb dxss smdlrm qslm hbff kcxl qhvszv cwkc wdxmfr cmvjrf
+#
+# QUESTÃO E) E no Texto B, quantos números mágicos existem e quais são? Imprima o número seguido da lista separada por espaços.
+#
+ABACATE = list("bct")
+ALPHABET = list("bctplmrsfzhjnwxkqvdg")
+
+NUMBER_MULTIPLER = 20
+
+prepositions = 0
+verbs = 0
+first_person_verbs = 0
+magic_numbers = []
+
+def is_preposition(word):
+    return word[0] in ABACATE and word[-1] in ABACATE
+
+def is_verb(word):
+    return len(word) == 7 and word[-1] not in ABACATE
+
+def is_first_person_verb(word):
+    return is_verb(word) and word[0] not in ABACATE
+
+def to_integer(word):
+    integer_value = 0
+
+    for index in range(len(word)):
+        integer_value += (ALPHABET.index(word[index]) * (NUMBER_MULTIPLER ** index))
+
+    return integer_value
+
+def is_magic_number(word):
+    return len(word) < 7 and len(word) % 2 == 0 and to_integer(word) % 13 == 0
+
+def sort_googlon_text(text):
+    human_text = []
+    googlon_text = []
+
+    # converte o texto googlon para texto humano
+    #
+    for word in text:
+        human_word = ""
+
+        for letter in word:
+            human_word += chr(ALPHABET.index(letter) + 65)
+
+        human_text.append(human_word)
+
+    # converte o texto humano ordenado para texto googlon
+    #
+    for word in sorted(human_text):
+        googlon_word = ""
+
+        for letter in word:
+            googlon_word += ALPHABET[ord(letter) - 65]
+
+        googlon_text.append(googlon_word)
+
+    return googlon_text
+
+for word in text_b:
+    if is_preposition(word):
+        prepositions += 1
+
+    if is_verb(word):
+        verbs += 1
+
+    if is_first_person_verb(word):
+        first_person_verbs += 1
+
+    if is_magic_number(word):
+        magic_numbers.append(word)
+
+print("## GDD 2011")
+print("##")
+print("## QUESTÃO A) No Texto B, quantas preposições existem? R: %d" % prepositions)
+print("## QUESTÃO B) Já no Texto B, quantos são os verbos? R: %d" % verbs)
+print("## QUESTÃO C) E quantos verbos do Texto B estão em primeira pessoa? R: %d" % first_person_verbs)
+print("## QUESTÃO D) Como seria a lista de vocabulário do Texto B? R: %s" % " ".join(sort_googlon_text(text_b)))
+print("## QUESTÃO E) E no Texto B, quantos números mágicos existem e quais são? Imprima o número seguido da lista separada por espaços. R: %d %s" % (len(magic_numbers), " ".join(magic_numbers)))
